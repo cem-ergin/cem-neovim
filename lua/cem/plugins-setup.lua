38002e85a5310668b27e10757e296c0b83712c0d
Cem Ergin <cem-ergin@outlook.com>
2023-01-28 00:10:17 +0000

    initial commit ready for react ready for flutter without tests

---
+++ lua/cem/plugins-setup.lua
@@ -0,0 +1,126 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
+
+return packer.startup(function(use)
+	use("wbthomason/packer.nvim")
+
+	-- lua functions that many plugins use
+	use("nvim-lua/plenary.nvim")
+
+	use("bluz71/vim-nightfly-guicolors")
+	use("folke/tokyonight.nvim")
+
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+
+	use("szw/vim-maximizer") -- maximizes and restores current window
+
+	-- essential plugins
+	use("tpope/vim-surround")
+	use("vim-scripts/ReplaceWithRegister")
+
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
+
+	-- icons
+	use("kyazdani42/nvim-web-devicons")
+
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
+
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
+
+	use("BurntSushi/ripgrep")
+	-- fuzzy finding
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+
+	-- autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+
+	-- managing & installing lsp servers
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use("jose-elias-alvarez/typescript.nvim")
+	use("onsails/lspkind.nvim")
+
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
+
+	use("David-Kunz/jester")
+
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
+
+	-- auto closing
+	use("windwp/nvim-autopairs")
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
+
+	use("lewis6991/gitsigns.nvim")
+
+	use({
+		"nvim-neotest/neotest",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"haydenmeade/neotest-jest",
+		},
+	})
+
+	-- jump in the file fast
+	use("ggandor/leap.nvim")
+
+	-- dart/flutter
+	use("dart-lang/dart-vim-plugin")
+	use("thosakwe/vim-flutter")
+	use("natebosch/vim-lsc")
+	use("natebosch/vim-lsc-dart")
+	use({ "neoclide/coc.nvim", branch = "release" })
+
+	-- flutter snippets
+	use("Nash0x7E2/awesome-flutter-snippets")
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
+end)
